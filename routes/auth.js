99ab8fcfc9a7d1789c6a6bff1593d73ef79baa30
Petr Kuzora <petr1725@yandex.ru>
2023-07-02 18:59:46 +0300

    Заменено регулярное выражение на константу

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { celebrate, Joi, Segments } = require('celebrate');
+const { regexpLink } = require('../utils/regexp');
 const { createUser, login } = require('../controllers/users');
 
 router.post('/signup', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^(https?:\/{2}|www\.)[-._~:/?#[\]@!$&'()*+,;=\w]+#?$/),
+    avatar: Joi.string().pattern(regexpLink),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
